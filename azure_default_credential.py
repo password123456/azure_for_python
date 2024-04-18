c72d43c3b062241f98c0f97512ed02bb339821ef
password123456 <sendmefuture@gmail.com>
2024-04-19 08:36:59 +0900

    Create azure_default_credential.py

---
+++ azure_default_credential.py
@@ -0,0 +1,36 @@
+
+import sys
+from azure.identity import DefaultAzureCredential
+from azure.core.exceptions import ClientAuthenticationError
+
+
+# you have to defind environment-variables
+# https://learn.microsoft.com/en-us/python/api/overview/azure/identity-readme?view=azure-python#environment-variables
+
+# AZURE_CLIENT_ID	ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET
+
+def main():
+    credentials = DefaultAzureCredential()
+
+    try:
+        token = credentials.get_token('https://management.azure.com/.default')
+        print("Access Token:", token.token)
+    except ClientAuthenticationError as ex:
+        print("Failed to authenticate:", ex.message)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        print(f'(Exception) Func:[{__name__.__name__}] '
+              f'Line:[{sys.exc_info()[-1].tb_lineno}] [{type(e).__name__}] {e}')
+
+
+"""
+RESULT
+
+Access Token: eyJ0eXAiOiJKV1QiLCJhbG....
+"""
